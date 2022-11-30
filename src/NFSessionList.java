2354258fba7903e7423238e9604be43604be07d4
Ray <51779943+coding-ray@users.noreply.github.com>
2022-11-30 17:15:07 +0800

    feat: combine sessions or add new session

--- src/NFSessionList.java
+++ src/NFSessionList.java
@@ -20,13 +20,36 @@ public class NFSessionList implements Iterable<NFSession> {
              * we store the previous session first,
              * and then create a new session.
              */
-            list.add(session);
-            session = new NFSession(val);
+            boolean isSessionAddedToList = combineOrAdd(session, val);
+            if (isSessionAddedToList)
+               session = new NFSession(val);
          }
       }
 
       // Add the last session
-      list.add(session);
+      combineOrAdd(session, new NFValue());
+   }
+
+   private boolean combineOrAdd(NFSession session, NFValue value) {
+      int index;
+
+      if ((index = getAddableSessionIndex(value)) == -1) {
+         list.add(session);
+         return true; // Added to list. "session" is added to list
+      }
+
+      NFSession storedSession = list.get(index);
+      storedSession.add(value);
+      list.set(index, storedSession);
+      return false; // Combined with the other session. "session" is not used.
+   }
+
+   private int getAddableSessionIndex(NFValue value) {
+      for (int i = list.size() - 1; i >= 0; i--) {
+         if (list.get(i).canAdd(value))
+            return i;
+      }
+      return -1;
    }
 
    @Override
